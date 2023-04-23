1a7b55182b07a8dd4db34884c6b4d0e37788712b
xfnw <xfnw@riseup.net>
2023-04-22 22:47:57 -0400

    fun

---
+++ axis.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from flutrad import *
+
+if len(sys.argv) > 1:
+    for i in range(MIDX+100):
+        graph(lambda x: i,scale=1,color='0')
+
+for i in range(10):
+    graph(lambda x: i,scale=MIDY//5,color='444444')
+
+for i in range(0,WIDTH,2):
+    put(i,MIDY,color='444444')
+for i in range(0,HEIGHT,2):
+    put(MIDX,i,color='444444')
+
