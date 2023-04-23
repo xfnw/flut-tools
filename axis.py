5294a2c018e98cdc68c548bf38774d5e2c34780a
xfnw <xfnw@riseup.net>
2023-04-23 14:01:26 -0400

    axis: do not waste time drawing zero-radius circle

--- axis.py
+++ axis.py
@@ -6,7 +6,7 @@ if len(sys.argv) > 1:
     for i in range(MIDX+100):
         graph(lambda x: i,scale=1,color='0')
 
-for i in range(10):
+for i in range(1,10):
     graph(lambda x: i,scale=MIDY//5,color='444444')
 
 for i in range(0,WIDTH,2):
