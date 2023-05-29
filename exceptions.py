9a24d934cdede534273a8ecfc10c69351f3875b8
xfnw <xfnw@riseup.net>
2023-05-29 19:50:29 -0400

    exceptions

---
+++ exceptions.py
@@ -0,0 +1,7 @@
+class CannotBeReused(Exception):
+    """
+    Raised when an action that cannot be done multiple times
+    was requested multiple times
+    """
+
+    pass
--- flutsync.py
+++ flutsync.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 
 import asyncio
-from collections import deque
 
-class Flutsync():
+from exceptions import CannotBeReused
+
+
+class Flutsync:
     def __init__(self, host: str, port: int):
-        self._first = False
+        self._already = False
         self._host = host
         self._port = port
+        self.width = 1024
+        self.height = 768
         self._cache = {}
-        self._queue = deque()
+        self._waiting = {}
         self.ttl = 32
 
+    async def connect(self):
+        if self._already:
+            raise CannotBeReused
+        self._already = true
 
+        asyncio.create_task(self._readerloop())
