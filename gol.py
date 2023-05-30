47c7bc84e51e45b5420f02547c76625e45a6f484
xfnw <xfnw@riseup.net>
2023-05-30 18:16:32 -0400

    gol

--- gol.py
+++ gol.py
@@ -19,9 +19,41 @@ async def prereq(flut, x, y):
     ct(flut.get(x, y + 1))
 
 
+async def neighbors(flut, x, y):
+    return [
+        ON == await flut.get(x - 1, y - 1),
+        ON == await flut.get(x, y - 1),
+        ON == await flut.get(x + 1, y - 1),
+        ON == await flut.get(x - 1, y),
+        ON == await flut.get(x + 1, y),
+        ON == await flut.get(x - 1, y + 1),
+        ON == await flut.get(x, y + 1),
+        ON == await flut.get(x + 1, y + 1),
+    ].count(True)
+
+
 async def gol(flut):
     await flut.has_size.wait()
-    await prereq(flut, 0, 0)
+
+    while True:
+        await flut.clear_cache()
+        await prereq(flut, -1, 0)
+        await prereq(flut, 0, 0)
+        await prereq(flut, 1, 0)
+
+        for y in range(flut.height):
+            for x in range(flut.width):
+                await prereq(flut, x + 2, y)
+                num = await neighbors(flut, x, y)
+
+                if await flut.get(x, y) == ON:
+                    if not (num == 2 or num == 3):
+                        print(f"{x},{y} dies")
+                        await flut.set(x, y, OFF)
+                else:
+                    if num == 3:
+                        print(f"{x},{y} is born")
+                        await flut.set(x, y, ON)
 
 
 async def main():
