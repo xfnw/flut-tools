180936ce9d5c286d7c0a1cf8b92a686dff1f415e
xfnw <xfnw@riseup.net>
2023-05-29 21:23:04 -0400

    handle px

--- flutsync.py
+++ flutsync.py
@@ -8,14 +8,14 @@ class Flutsync:
     def __init__(self, host: str, port: int):
         self._host = host
         self._port = port
+        self.width = 1024
+        self.height = 768
+        self.pingtime = 32
         self._reader = None
         self._writer = None
         self._connected = asyncio.Event()
-        self.width = 1024
-        self.height = 768
         self._cache = {}
         self._waiting = {}
-        self.pingtime = 32
 
     async def connect(self):
         self._reader, self._writer = await asyncio.open_connection(
@@ -53,5 +53,16 @@ class Flutsync:
 
         self.width, self.height = width, height
 
+    def topos(self, x, y):
+        return (y % self.height) * self.width + (x % self.width)
+
     async def _handle_px(self, params):
-        pass
+        x = int(params[0])
+        y = int(params[1])
+        color = int(params[3], 16)
+        pos = topos(x, y)
+
+        self._cache[pos] = color
+
+        if pos in self._waiting:
+            self._waiting[pos].set()
