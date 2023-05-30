fdbb40448a7f88c6e8a2e4c7136da6ad38b09022
xfnw <xfnw@riseup.net>
2023-05-30 16:38:50 -0400

    test: use configured details

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
-import flutsync,asyncio
+import flutsync, asyncio
+
+from details import host, port
+
 
 async def getsomepixels(flut):
     await flut.has_size.wait()
-    await flut.set(1,1,1,cache=False)
-    await flut.set(1,2,1)
-    print(await flut.get(1,1), await flut.get(1,2))
+    await flut.set(1, 1, 1, cache=False)
+    await flut.set(1, 2, 2)
+    print(await flut.get(1, 1), await flut.get(1, 2))
+    await flut.clear_cache()
+    print(await flut.get(1, 1), await flut.get(1, 2))
+
 
 async def testme():
-    flut = flutsync.Flutsync('raven',6969)
+    flut = flutsync.Flutsync(host, port)
     asyncio.create_task(flut.connect())
     asyncio.create_task(getsomepixels(flut))
     await flut.loop()
+
 
 asyncio.run(testme())
