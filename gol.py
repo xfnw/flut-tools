118493541812b8d2c924ae4192c0c2918d4553ad
xfnw <xfnw@riseup.net>
2023-08-07 11:40:40 -0400

    gol: step support

--- gol.py
+++ gol.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import flutsync, asyncio
+import sys, flutsync, asyncio
 from asyncio import create_task as ct
 
 from details import host, port
@@ -9,20 +9,20 @@ ON = 0x00FF00
 OFF = 0x0
 
 
-async def neighbors(flut, x, y):
+async def neighbors(flut, x, y, step=1):
     return [
-        ON == await flut.get(x - 1, y - 1),
-        ON == await flut.get(x, y - 1),
-        ON == await flut.get(x + 1, y - 1),
-        ON == await flut.get(x - 1, y),
-        ON == await flut.get(x + 1, y),
-        ON == await flut.get(x - 1, y + 1),
-        ON == await flut.get(x, y + 1),
-        ON == await flut.get(x + 1, y + 1),
+        ON == await flut.get(x - 1, y - 1, step=step),
+        ON == await flut.get(x, y - 1, step=step),
+        ON == await flut.get(x + 1, y - 1, step=step),
+        ON == await flut.get(x - 1, y, step=step),
+        ON == await flut.get(x + 1, y, step=step),
+        ON == await flut.get(x - 1, y + 1, step=step),
+        ON == await flut.get(x, y + 1, step=step),
+        ON == await flut.get(x + 1, y + 1, step=step),
     ].count(True)
 
 
-async def gol(flut):
+async def gol(flut, step=1):
     await flut.has_size.wait()
 
     iteration = 0
@@ -31,29 +31,33 @@ async def gol(flut):
         print("iteration", iteration := iteration + 1)
 
         await flut.clear_cache()
-        await flut.cache_row(-1)
-        await flut.cache_row(0)
+        await flut.cache_row(-1, step=step)
+        await flut.cache_row(0, step=step)
 
-        for y in range(flut.height):
+        for y in range(flut.height // step):
             # print("row", y)
-            await flut.cache_row(y + 1)
-            for x in range(flut.width):
+            await flut.cache_row(y + 1, step=step)
+            for x in range(flut.width // step):
                 num = await neighbors(flut, x, y)
 
-                if await flut.get(x, y) == ON:
+                if await flut.get(x, y, step=step) == ON:
                     if not (num == 2 or num == 3):
                         # print(f"{x},{y} dies")
-                        await flut.set(x, y, OFF, cache=False)
+                        await flut.set(x, y, OFF, cache=False, step=step)
                 else:
                     if num == 3:
                         # print(f"{x},{y} is born")
-                        await flut.set(x, y, ON, cache=False)
+                        await flut.set(x, y, ON, cache=False, step=step)
 
 
 async def main():
+    step = 1
+    if len(sys.argv) > 1:
+        step = int(sys.argv[1])
+
     flut = flutsync.Flutsync(host, port)
     await flut.connect()
-    asyncio.create_task(gol(flut))
+    asyncio.create_task(gol(flut, step=step))
     await flut.loop()
 
 
