4f842a50fdca5ccc3233c716e2457bf640b50172
xfnw <xfnw@riseup.net>
2023-09-29 20:02:35 -0400

    waves: allow setting phase

--- waves.py
+++ waves.py
@@ -2,10 +2,16 @@
 
 from flutrad import *
 
-o = [(500,500,8),(530,500,8),(560,500,8)]
+o = [(500,500),(530,500),(560,500)]
 
 if len(sys.argv) > 1:
     o = eval(sys.argv[1])
+
+for k, v in enumerate(o):
+    if len(v) == 2:
+        o[k] = v + (8,0)
+    elif len(v) == 3:
+        o[k] = v + (0,)
 
 m = 128 / len(o)
 
@@ -13,8 +19,8 @@ m = 128 / len(o)
 for y in range(HEIGHT):
     for x in range(WIDTH):
         p = 0
-        for k, v, s in o:
-            d = sqrt((k - x) ** 2 + (v - y) ** 2)
-            p += cos(d/s)
-        p = int(p * m + 127) * 65793
+        for k, v, s, h in o:
+            d = sqrt((k-x) ** 2 + (v-y) ** 2)
+            p += cos(d/s + h)
+        p = int(p*m + 127) * 65793
         print(f"PX {x} {y} {p:x}")
