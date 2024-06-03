c7675d33d7c386c4658debb3d98e0bc54fbe3854
xfnw <xfnw@riseup.net>
2024-06-03 16:50:28 -0400

    storeflut: add simple cli

--- storeflut/src/main.rs
+++ storeflut/src/main.rs
@@ -1,21 +1,57 @@
-use primative::MemorySlab;
-use std::sync::Arc;
+use std::{
+    env::args,
+    io::{stdin, stdout, Read, Write},
+    sync::Arc,
+};
 use tokio::net::TcpStream;
+
+use crate::primative::MemorySlab;
 
 pub mod primative;
 pub mod protocol;
 
 #[tokio::main]
 async fn main() {
-    let stream = TcpStream::connect("feesh:1234").await.unwrap();
+    let argv: Vec<_> = args().skip(1).collect();
+    if !(2..=3).contains(&argv.len()) {
+        println!("usage: host offset [length]");
+        return;
+    }
+
+    let stream = TcpStream::connect(argv[0].as_str()).await.unwrap();
     let slab = Arc::new(MemorySlab::new(stream));
     let newslab = slab.clone();
     tokio::spawn(async move {
         newslab.start().await;
     });
-    println!("{:?}", slab.get_pixel(1).await);
-    slab.set(0, Vec::from_iter(0..=255).as_slice())
-        .await
-        .unwrap();
-    println!("{:?}", slab.get(0, 256).await.unwrap());
+
+    if argv.len() == 3 {
+        let mut offset = argv[1].parse().unwrap();
+        let mut length = argv[2].parse().unwrap();
+        let mut out = stdout();
+
+        while length > 1024 {
+            out.write_all(slab.get(offset, 1024).await.unwrap().as_ref())
+                .unwrap();
+            length -= 1024;
+            offset += 1024;
+        }
+
+        out.write_all(slab.get(offset, length).await.unwrap().as_ref())
+            .unwrap();
+        return;
+    }
+
+    let mut buf = [0_u8; 1024];
+    let mut offset = argv[1].parse().unwrap();
+    let mut inp = stdin();
+
+    while let Ok(len) = inp.read(&mut buf) {
+        if len == 0 {
+            break;
+        }
+
+        slab.set(offset, &buf[..len]).await.unwrap();
+        offset += len as u32;
+    }
 }
